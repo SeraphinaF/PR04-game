8ca160a6ae3724bf3324e018f7f52159501e88b2
SeraphinaF <1028267@hr.nl>
2023-05-09 15:43:46 +0200

    derde commit

--- src/js/game.ts
+++ src/js/game.ts
@@ -12,16 +12,11 @@ export class Game {
 
     constructor() {
         this.engine = new Engine({ width: 500, height: 500 });
-        this.engine.start(ResourceLoader).then(() => {
-            this.startGame();
-            this.gameloop();
-        })
+        this.engine.start(ResourceLoader).then(() => this.gameloop());
+    
 
         this.obstacle_manager = new ObstacleManager(this.engine);
         this.bird = new Bird(this.engine, 150, 460, 50, 50, Color.Green);
-
-        this.check_keypress();
-        //update all actors and calls gameloop once per frame
     }
 
     startGame() {
@@ -35,24 +30,27 @@ export class Game {
         this.engine.add(ground);
     }
 
-    spaceKeyPressed:boolean = false;
-
-    check_keypress() {
+    
+    
+    check_keypress():Boolean{
         this.engine.input.keyboard.on("press", (event: KeyEvent)=> {
           if (event.key === Input.Keys.Space) {
-              console.log('press')
-              this.spaceKeyPressed = true
-            }
-        })
-    }
+             console.log('jump')
+              return true;
+        }
+    })
+}
+
+    
 
     gameloop() {
-        while (true) {
-            if(this.spaceKeyPressed === true){
+        this.startGame();
+        
+            if(this.check_keypress()){
                 console.log('it is really happening!')
             }
         }
     }
-}
+
 
 new Game()
